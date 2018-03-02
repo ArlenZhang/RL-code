be828ca84036e219934c2a48ba70165007d0443b
ArlenZhang <longyin128128@163.com>
2018-03-02 15:29:11 +0800

    add

--- DQN/RL_brain.py
+++ DQN/RL_brain.py
@@ -1,204 +1,203 @@
 """
-    This part of code is the DQN brain, which is a brain of the agent.
-    All decisions are made in here.
-    Using Tensorflow to build the neural network.
-"""
+    搭建流程：
+        get_date() : 更新记忆库数据，供训练使用
+        create_logits() : 直接将模型搭建和数据(get_data准备的数据存储在类变量域)结合在一起，得到logits输出
+        create_loss() : 同神经网络使用
+        create_optimize() : ...
+    作者: Longyin Zhang
+    日期: 2018.3.2
 
+"""
 import numpy as np
 import tensorflow as tf
+import util
 np.random.seed(1)
 tf.set_random_seed(1)
 
-# Deep Q Network off-policy
+"""
+    Deep Q Network 类
+"""
 class DeepQNetwork:
-    def __init__(
-            self,
-            n_actions,
-            n_features,
-            learning_rate=0.01,
-            reward_decay=0.9,
-            e_greedy=0.9,
-            replace_target_iter=300,
-            memory_size=500,
-            batch_size=32,
-            e_greedy_increment=None,
-            output_graph=False
-    ):
-        # 初始化超参数
+    def __init__(self, n_actions, n_features, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9,
+                 replace_target_iter=300, memory_size=500, batch_size=32, e_greedy_increment=None, output_graph=False):
+        self.cost = self.memory_counter = self._train_op = self.loss = self.q_target = self.s_ = self.s = self.sess = \
+            self.replace_target_op = None
+        # ========== 初始化模型的超参数
+        self.lr = learning_rate  # 学习效率
+        self.batch_size = batch_size  # 批处理量
+        self.c_names_e = ['eval_net_params', tf.GraphKeys.GLOBAL_VARIABLES]
+        self.c_names_t = ['target_net_params', tf.GraphKeys.GLOBAL_VARIABLES]
+        self.n_l1 = 10  # 第一层神经元个数
+        self.w_initializer = tf.random_normal_initializer(0., 0.3)
+        self.b_initializer = tf.constant_initializer(0.1)
+
+        # ========== 配置QLearning计算过程中用到的参数
+        self.gamma = reward_decay   # 反馈衰减值
+        self.epsilon_max = e_greedy  # 决定选择使用最优action还是随机action的比重
+        self.epsilon_increment = e_greedy_increment  # 随模型训练时间增加而越发偏向最优action选择，增大epsilon
+        self.epsilon = 0 if e_greedy_increment is not None else self.epsilon_max
+
         self.n_actions = n_actions  # 可选动作
         self.n_features = n_features  # 观察值转成n_features列的数据
-        self.lr = learning_rate  # 学习效率
-        self.gamma = reward_decay   # 反馈衰减
-        self.epsilon_max = e_greedy  # epsilon的数据选择使用最优action
-        self.replace_target_iter = replace_target_iter  # 学习隔300步更新一次target值
+        self.replace_target_iter = replace_target_iter  # 学习隔replace_target_iter步更新一次target值
         self.memory_size = memory_size  # 记忆库的大小
-        self.batch_size = batch_size  # 批处理量
-        self.epsilon_increment = e_greedy_increment  # 模型训练一段时间，越来越偏向最优action选择，所以增大epsilon
-        # 如果epsilon不是0那么允许epsilon从0开始累加，否则用固定值0.9
-        self.epsilon = 0 if e_greedy_increment is not None else self.epsilon_max
-        # 全局学习计数，learn一次就累加1
-        self.learn_step_counter = 0
 
-        # 创建记忆库 initialize zero memory [s, a, r, s_]
-        self.memory = np.zeros((self.memory_size, n_features * 2 + 2))
+        # ========== 初始化全局变量
+        self.learn_step_counter = 0  # learn一次就累加1
+        self.memory = np.zeros((self.memory_size, n_features * 2 + 2))  # 创建初始记忆库
+        self.cost_steps = []  # 存储各个批次网络的误差值
 
-        # consist of [target_net, evaluate_net]
-        self._build_net()
+        # ========== 初始化模型
+        self.build()
+
+        # ========== tensorflow 会话
+        self.session_summary(output_graph=output_graph)
+
+    def build(self):
+        # 为之前观察值 state 创建place_holder作为eval网络的输入
+        self.s = tf.placeholder(tf.float32, [None, self.n_features], name='s')
+        # 采取action之后下一个状态的观察值state_创建place_holder作为target网络的输入
+        self.s_ = tf.placeholder(tf.float32, [None, self.n_features], name='s_')
+        # 为真实的Q值创建placeholder 用于对比q_predict计算损失值
+        self.q_target = tf.placeholder(tf.float32, [None, self.n_actions], name='Q_target')
+
+        self._build_net()  # 创建网络
+
+        self.create_loss_opt()  # 创建计算loss和optimizer的操作
+
+        self.update_target_params()  # 创建更新target网络参数的操作
+
+    """
+        创建两个结构相同参数不同的神经网络 target_n 和 eval_n
+    """
+    def _build_net(self):
+        # 创建评估网络
+        with tf.variable_scope('eval_net'):
+            # 第一层网络
+            with tf.variable_scope('l1'):
+                w1 = tf.get_variable('w1', [self.n_features, self.n_l1], initializer=self.w_initializer,
+                                     collections=self.c_names_e)
+                b1 = tf.get_variable('b1', [1, self.n_l1], initializer=self.b_initializer, collections=self.c_names_e)
+                l1 = tf.nn.relu(tf.matmul(self.s, w1) + b1)
+
+            # 第二层神经网络
+            with tf.variable_scope('l2'):
+                w2 = tf.get_variable('w2', [self.n_l1, self.n_actions], initializer=self.w_initializer,
+                                     collections=self.c_names_e)
+                b2 = tf.get_variable('b2', [1, self.n_actions], initializer=self.b_initializer,
+                                     collections=self.c_names_e)
+                self.q_eval = tf.matmul(l1, w2) + b2
+
+        # 创建target目标神经网络
+        with tf.variable_scope('target_net'):
+            # 第一层网络
+            with tf.variable_scope('l1'):
+                w1 = tf.get_variable('w1', [self.n_features, self.n_l1], initializer=self.w_initializer,
+                                     collections=self.c_names_t)
+                b1 = tf.get_variable('b1', [1, self.n_l1], initializer=self.b_initializer, collections=self.c_names_t)
+                l1 = tf.nn.relu(tf.matmul(self.s_, w1) + b1)
+
+            # 第二层神经网络
+            with tf.variable_scope('l2'):
+                w2 = tf.get_variable('w2', [self.n_l1, self.n_actions], initializer=self.w_initializer,
+                                     collections=self.c_names_t)
+                b2 = tf.get_variable('b2', [1, self.n_actions], initializer=self.b_initializer,
+                                     collections=self.c_names_t)
+                self.q_next = tf.matmul(l1, w2) + b2
+
+    # 计算损失
+    def create_loss_opt(self):
+        # 定义损失函数，也就是QLearning中的(Q_target - Q_predict)
+        with tf.variable_scope('loss'):
+            self.loss = tf.reduce_mean(tf.squared_difference(self.q_target, self.q_eval))
+        # optimizer 反向传播过程，学习神经网络参数
+        with tf.variable_scope('train'):
+            self._train_op = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
+
+    # 用Eval网络参数更新Target网络参数
+    def update_target_params(self):
+        # 定义获取target和eval网络的参数 w 和 b的操作
         t_params = tf.get_collection('target_net_params')
         e_params = tf.get_collection('eval_net_params')
         self.replace_target_op = [tf.assign(t, e) for t, e in zip(t_params, e_params)]
 
+    # tensorflow 会话
+    def session_summary(self, output_graph):
         self.sess = tf.Session()
-
         if output_graph:
-            # $ tensorboard --logdir=logs
-            # tf.train.SummaryWriter soon be deprecated, use following
-            tf.summary.FileWriter("../../graphs/", self.sess.graph)
+            tf.summary.FileWriter("../../graphs/DQN/", self.sess.graph)
 
         self.sess.run(tf.global_variables_initializer())
-        self.cost_his = []
 
-    # 重点掌握网络搭建
-    def _build_net(self):
-        # ------------------ build evaluate_net 网络1 两层的神经网络 ------------------
-        self.s = tf.placeholder(tf.float32, [None, self.n_features], name='s')  # input 输入的是state 输出各个操作Q值
-        self.q_target = tf.placeholder(tf.float32, [None, self.n_actions], name='Q_target')  # for calculating loss
-        with tf.variable_scope('eval_net'):
-            # c_names(collections_names) are the collections to store variables
-            # n_l1代表第一层的神经元个数
-            c_names, n_l1, w_initializer, b_initializer = \
-                ['eval_net_params', tf.GraphKeys.GLOBAL_VARIABLES], 10, \
-                tf.random_normal_initializer(0., 0.3), tf.constant_initializer(0.1)  # config of layers
+    """
+        QLearning 算法应用部分
+    """
+    def learn(self):
+        # 学习每经过300次,更换一次target网络参数
+        if self.learn_step_counter % self.replace_target_iter == 0:
+            self.sess.run(self.replace_target_op)
+            print('\ntarget参数被更新\n')
 
-            # first layer. collections is used later when assign to target net
-            with tf.variable_scope('l1'):
-                w1 = tf.get_variable('w1', [self.n_features, n_l1], initializer=w_initializer, collections=c_names)
-                b1 = tf.get_variable('b1', [1, n_l1], initializer=b_initializer, collections=c_names)
-                l1 = tf.nn.relu(tf.matmul(self.s, w1) + b1)
+        # 从记忆库中选择batch_size的数据学习，这是RL应用神经网络的关键
+        # arr中每一行的存储: self.memory[index, :] = np.hstack((s_last, [action, reward], s_temp))
+        if self.memory_counter > self.memory_size:
+            sample_index = np.random.choice(self.memory_size, size=self.batch_size)
+        else:
+            sample_index = np.random.choice(self.memory_counter, size=self.batch_size)
+        batch_memory = self.memory[sample_index, :]
 
-            # second layer. collections is used later when assign to target net
-            with tf.variable_scope('l2'):
-                w2 = tf.get_variable('w2', [n_l1, self.n_actions], initializer=w_initializer, collections=c_names)
-                b2 = tf.get_variable('b2', [1, self.n_actions], initializer=b_initializer, collections=c_names)
-                self.q_eval = tf.matmul(l1, w2) + b2
-        # 定义损失函数，也就是QLearning中的(Q_target - Q_predict)
-        with tf.variable_scope('loss'):
-            self.loss = tf.reduce_mean(tf.squared_difference(self.q_target, self.q_eval))
-        with tf.variable_scope('train'):
-            self._train_op = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
+        # 从batch_memory中提取四组信息 state, action, reward, state_
+        states_, states, actions, rewards = util.get_data(batch_memory, self.n_features)
 
-        # ------------------ build target_net 网络2 ------------------
-        self.s_ = tf.placeholder(tf.float32, [None, self.n_features], name='s_')    # input
-        with tf.variable_scope('target_net'):
-            # c_names(collections_names) are the collections to store variables
-            c_names = ['target_net_params', tf.GraphKeys.GLOBAL_VARIABLES]
+        # 对当前随机参数的模型输入state根据原有的模型参数计算得到两组q值，分别为 Q评估 和 Q现实
+        # 返回的是状态对应的分值，相当于查Q表的功能，用两个网络作为一个Q表能力更好
+        q_next, q_eval = self.sess.run(
+            [self.q_next, self.q_eval],
+            feed_dict={
+                self.s_: states_,  # 每个训练记录的state_值
+                self.s: states,    # 每个训练记录的state 值
+            })
 
-            # first layer. collections is used later when assign to target net
-            with tf.variable_scope('l1'):
-                w1 = tf.get_variable('w1', [self.n_features, n_l1], initializer=w_initializer, collections=c_names)
-                b1 = tf.get_variable('b1', [1, n_l1], initializer=b_initializer, collections=c_names)
-                l1 = tf.nn.relu(tf.matmul(self.s_, w1) + b1)
+        # Sarsa算法计算Q_target
+        q_target = q_eval.copy()
+        batch_index = np.arange(self.batch_size, dtype=np.int32)
+        q_target[batch_index, actions] = rewards + self.gamma * np.max(q_next, axis=1)
 
-            # second layer. collections is used later when assign to target net
-            with tf.variable_scope('l2'):
-                w2 = tf.get_variable('w2', [n_l1, self.n_actions], initializer=w_initializer, collections=c_names)
-                b2 = tf.get_variable('b2', [1, self.n_actions], initializer=b_initializer, collections=c_names)
-                self.q_next = tf.matmul(l1, w2) + b2
+        # 将states, q_target送入神经网络, states再次经神经网络计算的到q_eval 再与q_target计算loss值并训练网络
+        _, self.cost = self.sess.run([self._train_op, self.loss], feed_dict={self.s: states, self.q_target: q_target})
+        self.cost_steps.append(self.cost)
 
-    # 存储转移数据到记忆库
+        # 提高e-greedy的epsilon值，提高对模型的信任
+        self.epsilon = self.epsilon + self.epsilon_increment if self.epsilon < self.epsilon_max else self.epsilon_max
+        self.learn_step_counter += 1
+
+    # 更新记忆库
     def store_transition(self, s_last, action, reward, s_temp):
-        if not hasattr(self, 'memory_counter'):
+        if not hasattr(self, 'memory_counter') or self.memory_counter is None:
             self.memory_counter = 0
-
         transition = np.hstack((s_last, [action, reward], s_temp))
-        # update the memory
         index = self.memory_counter % self.memory_size
         self.memory[index, :] = transition
         self.memory_counter += 1
 
-    # 选择动作
+    """
+            选择根据观察值 state，将state数据作为eval神经网络的输入数据， 
+        输出不同action的，取最大value对应的action.
+    """
     def choose_action(self, observation):
-        # 将1维观察值转成2维,因为神经网络设置的nfeatures就是2列的数据
+        # 将1维观察值转成2维,神经网络设置的nfeatures就是2
         observation = observation[np.newaxis, :]
-        # 90% 最优选择
+        # 90% 最优
         if np.random.uniform() < self.epsilon:
-            # forward feed the observation and get q value for every actions
             actions_value = self.sess.run(self.q_eval, feed_dict={self.s: observation})
             action = np.argmax(actions_value)
         else:
             action = np.random.randint(0, self.n_actions)
         return action
 
-    # 学习的是神经网络参数而不是Q表格
-    def learn(self):
-        # 学习每经过300次,更换一次target
-        if self.learn_step_counter % self.replace_target_iter == 0:
-            self.sess.run(self.replace_target_op)
-            print('\ntarget_params_replaced\n')
-
-        # 从所有的记忆中采样部分,从记忆中学习
-        if self.memory_counter > self.memory_size:
-            sample_index = np.random.choice(self.memory_size, size=self.batch_size)
-        else:
-            sample_index = np.random.choice(self.memory_counter, size=self.batch_size)
-        batch_memory = self.memory[sample_index, :]
-        # 对模型输入state根据原有的模型参数计算得到两组q值，分别为Q评估和Q现实
-        q_next, q_eval = self.sess.run(
-            [self.q_next, self.q_eval],
-            feed_dict={
-                # arr中每一行的存储: self.memory[index, :] = np.hstack((s_last, [action, reward], s_temp))
-                self.s_: batch_memory[:, -self.n_features:],  # fixed params
-                self.s: batch_memory[:, :self.n_features],  # newest params
-            })
-
-        # 下面就是QLearning的算法逻辑使用, q_eval代表Q评估, q_target是Q现实，从记忆库中得到
-        q_target = q_eval.copy()
-        batch_index = np.arange(self.batch_size, dtype=np.int32)
-        eval_act_index = batch_memory[:, self.n_features].astype(int)  # 每一行的第二列是对应action
-        reward = batch_memory[:, self.n_features + 1]  # 每一行的第三列是reward
-
-        # 由此可见，DQN是Sarsa算法+神经网络而不是QLearning+神经网络
-        q_target[batch_index, eval_act_index] = reward + self.gamma * np.max(q_next, axis=1)
-
-        """
-            For example in this batch I have 2 samples and 3 actions:
-            q_eval =
-            [[1, 2, 3],
-             [4, 5, 6]]
-    
-            q_target = q_eval =
-            [[1, 2, 3],
-             [4, 5, 6]]
-    
-            Then change q_target with the real q_target value w.r.t the q_eval's action.
-            For example in:
-                sample 0, I took action 0, and the max q_target value is -1;
-                sample 1, I took action 2, and the max q_target value is -2:
-            q_target =
-            [[-1, 2, 3],
-             [4, 5, -2]]
-    
-            So the (q_target - q_eval) becomes:
-            [[(-1)-(1), 0, 0],
-             [0, 0, (-2)-(6)]]
-    
-            We then backpropagate this error w.r.t the corresponding action to network,
-            leave other action as error=0 cause we didn't choose it.
-        """
-
-        # train eval network
-        _, self.cost = self.sess.run([self._train_op, self.loss],
-                                     feed_dict={self.s: batch_memory[:, :self.n_features],
-                                                self.q_target: q_target})
-        self.cost_his.append(self.cost)
-
-        # 每次学习一次就提高一点用于10%随机选择的Epsilon来提高决策能力,信任网络参数在越来越可靠
-        self.epsilon = self.epsilon + self.epsilon_increment if self.epsilon < self.epsilon_max else self.epsilon_max
-        self.learn_step_counter += 1
-
     def plot_cost(self):
         import matplotlib.pyplot as plt
-        plt.plot(np.arange(len(self.cost_his)), self.cost_his)
+        plt.plot(np.arange(len(self.cost_steps)), self.cost_steps)
         plt.ylabel('Cost')
         plt.xlabel('training steps')
         plt.show()
--- DQN/run_this.py
+++ DQN/run_this.py
@@ -11,23 +11,18 @@ def run_maze():
         while True:
             # fresh env
             env.render()
-            env.after(100)
             # RL choose action based on observation
             # 不再通过Q表选取动作，而是把当前观察值作为神经网络参数计算得到一组Q值，取最优
             action = RL.choose_action(observation)
-
             # RL take action and get next observation and reward
             observation_, reward, done = env.step(action)
             # 记忆库在不断更新，最近的知识更重要，替换了以往知识
             RL.store_transition(observation, action, reward, observation_)
-            # 200步骤以前都是在建立记忆库(只是存储数据环境+操作), 200 以后每5步学习一次
+            # 200以前都是在建立记忆库(只是存储数据环境+操作), 200 以后每5步学习一次
             if (step > 200) and (step % 5 == 0):
                 RL.learn()
-
             # 将当前观察值更新
             observation = observation_
-
-            # break while loop when end of this episode
             if done:
                 break
             step += 1
@@ -36,7 +31,6 @@ def run_maze():
     print('game over')
     env.destroy()
 
-
 if __name__ == "__main__":
     # maze game
     env = Maze()
@@ -44,12 +38,16 @@ if __name__ == "__main__":
                       learning_rate=0.01,
                       reward_decay=0.9,
                       e_greedy=0.9,
+                      # e_greedy_increment=0.001,
                       replace_target_iter=200,
                       memory_size=2000,
                       output_graph=True
                       )
-    env.after(100, run_maze)
+    env.after(10, run_maze)
     env.mainloop()
     RL.plot_cost()
-    # 在tensorboard中观察结果
-    # tensorboard --logdir="/home/arlenzhang/Desktop/RL/RL_Pro/logs"
+
+"""
+    在tensorboard中观察结果
+    tensorboard --logdir="/home/arlenzhang/Desktop/RL/RL_Pro/logs"
+"""
